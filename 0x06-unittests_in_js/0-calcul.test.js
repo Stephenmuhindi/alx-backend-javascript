c645b97f83ba5f96d9b7f0f57c36cc4b5c5eaa4e
Stephenmuhindi <bteneson42@gmail.com>
2024-06-20 04:59:40 -0700

    swee

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,25 @@
+ it(`checking if numbers round`, function() {
+      const res = calculateNumber(1, 2);
+      assert.strictEqual(res, 3);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1, 3.7);
+      assert.strictEqual(res, 5);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1.2, 3.7);
+      assert.strictEqual(res, 5);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(6, 9);
+      assert.strictEqual(res, 15);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(1.5, 3.7);
+      assert.strictEqual(res, 6);
+    });
+    it(`checking if numbers round`, function() {
+      const res = calculateNumber(4, 3);
+	assert.strictEqual(res, 7);
+    });
+});
