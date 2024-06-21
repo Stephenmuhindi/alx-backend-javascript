6bbe6cc729b9e6c40ba627580df1c908b6f58393
Stephenmuhindi <benteneson42@gmail.com>
2024-06-21 12:24:20 +0000

    tete

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -5,11 +5,7 @@ describe('API integration test', () => {
   const API_URL = 'http://localhost:7865';
 
   it('GET / returns correct response', (done) => {
-    request.get(`${API_URL}/`, (err, res, body) => {
-      if (err) {
-        console.error('Error:', err);
-        return done(err);
-      }
+    request.get(`${API_URL}/`, (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Welcome to the payment system');
       done();
@@ -17,37 +13,24 @@ describe('API integration test', () => {
   });
 
   it('GET /cart/:id returns correct response for valid :id', (done) => {
-    request.get(`${API_URL}/cart/47`, (err, res, body) => {
-      if (err) {
-        console.error('Error:', err);
-        return done(err);
-      }
+    request.get(`${API_URL}/cart/50`, (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
-      expect(body).to.be.equal('Payment methods for cart 47');
+      expect(body).to.be.equal('Payment methods for cart 50');
       done();
     });
   });
 
-  it('GET /cart/:id returns 404 response for negative number values in :id', (done) => {
-    request.get(`${API_URL}/cart/-47`, (err, res) => {
-      if (err) {
-        console.error('Error:', err);
-        return done(err);
-      }
+  it('GET /cart/:id returns 404 for negative integer :id', (done) => {
+    request.get(`${API_URL}/cart/-50`, (_err, res, _body) => {
       expect(res.statusCode).to.be.equal(404);
       done();
     });
   });
 
   it('GET /cart/:id returns 404 response for non-numeric values in :id', (done) => {
-    request.get(`${API_URL}/cart/d200-44a5-9de6`, (err, res) => {
-      if (err) {
-        console.error('Error:', err);
-        return done(err);
-      }
+    request.get(`${API_URL}/cart/ddf`, (_err, res, _body) => {
       expect(res.statusCode).to.be.equal(404);
       done();
     });
   });
 });
-
